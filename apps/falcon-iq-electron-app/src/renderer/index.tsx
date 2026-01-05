ba65e575ff0f2ae3a03b20bf8c67892d0e55e16f
Barry Steyn <barry.steyn@nimrox.ai>
2026-01-05 14:00:52 -0800

    fix: eslint non-null assertion fix

--- apps/falcon-iq-electron-app/src/renderer/index.tsx
+++ apps/falcon-iq-electron-app/src/renderer/index.tsx
@@ -2,6 +2,9 @@ import '@styles/globals.css';
 import { createRoot } from 'react-dom/client';
 import App from '@app/App';
 
-const container = document.getElementById('root')!;
+const container = document.getElementById('root');
+if (!container) {
+  throw new Error('Root element not found');
+}
 const root = createRoot(container);
 root.render(<App />);
