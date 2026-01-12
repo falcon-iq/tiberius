bf836cf0e717bedaa4a01ea6a08ab0ace7987e9f
Barry Steyn <barry.steyn@nimrox.ai>
2026-01-11 18:19:10 -0800

    feat: settings modal

---
+++ libs/shared/ui/jest.config.ts
@@ -0,0 +1,18 @@
+export default {
+  displayName: 'shared-ui',
+  preset: '../../../jest.preset.js',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.[tj]sx?$': [
+      '@swc/jest',
+      {
+        jsc: {
+          parser: { syntax: 'typescript', tsx: true },
+          transform: { react: { runtime: 'automatic' } },
+        },
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/libs/shared/ui',
+};
